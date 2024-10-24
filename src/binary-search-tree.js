30f616d56f7bac05baa4e1adff136e6ef80e6b23
iribyz <byzovairina@gmail.com>
2024-10-25 01:00:46 +0300

    feat: add solution for bst

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -60,8 +60,7 @@ class BinarySearchTree {
 
   find(data) {
     let currentEl = this.tree;
-    let elementExists = false;
-    while (currentEl && (elementExists === false)) {
+    while (currentEl) {
       if (data === currentEl.data) {
         return currentEl;
       } else {
@@ -72,22 +71,84 @@ class BinarySearchTree {
         }
       }
     }
-    return null;
+    return currentEl;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    let currentEl = this.tree;
+    let nextEl = this.tree;
+  
+    while (nextEl) {
+      if (nextEl.data === data) { 
+        if ((nextEl.left === null) && (nextEl.right === null)) {
+          if (currentEl.left?.data === data) {
+            currentEl.left = null;
+          } else {
+            currentEl.right = null;
+          }
+        } 
+        if ((nextEl.left === null) && (nextEl.right !== null)) {
+          if (currentEl.left?.data === data) {
+            currentEl.left = nextEl.right;
+          } else {
+            currentEl.right = nextEl.right;
+          }
+        } 
+        if ((nextEl.left !== null) && (nextEl.right === null)) {
+          if (currentEl.left?.data === data) {
+            currentEl.left = nextEl.left;
+          } else {
+            currentEl.right = nextEl.left;
+          }
+        } 
+        //////////////////////
+        if ((nextEl.left !== null) && (nextEl.right !== null)) {
+          //find min element from right side
+          let el1 = nextEl.right;
+          let el2 = el1.left;
+          if (el2 === null) {
+            el1.left = nextEl.left;
+            nextEl = el1;
+          } else {
+            while (el2.left) {
+              el1 = el2;
+              el2 = el2.left;
+            }
+            nextEl.data = el2.data;
+            el1.left = el2.right;
+          }
+        } 
+        ///////////////////
+        break;
+      } else { 
+        currentEl = nextEl;
+        if (data < nextEl.data) {
+          nextEl = nextEl.left;
+        } else {
+          nextEl = nextEl.right;
+        }      
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let currentEl = this.tree;
+    let minEl;
+    while (currentEl) {
+      minEl = currentEl.data; 
+      currentEl = currentEl.left;
+    }
+    return minEl;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let currentEl = this.tree
+    let maxEl;
+    while (currentEl) {
+      maxEl = currentEl.data; 
+      currentEl = currentEl.right;
+    }
+    return maxEl;
   }
 }
 
